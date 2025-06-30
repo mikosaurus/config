e89325aaa97ea216b494588b293fe6c0c2b22f68
Anders Johannessen <slatleim1991@gmail.com>
2025-06-30 14:04:44 +0000

    added .local/bin to path

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -4,6 +4,7 @@ export PATH=~/nvim/bin:$PATH
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
 
 export PATH=~/.local/share/nvim/mason/bin:$PATH
 export EDITOR=nvim
