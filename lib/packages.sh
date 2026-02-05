9f3eeecec6378db0fbc7b1a3d376e80b8b613cc6
Anders Johannessen <slatleim1991@gmail.com>
2026-02-05 18:56:09 +0100

    added pkg keychain to pacman

--- lib/packages.sh
+++ lib/packages.sh
@@ -15,6 +15,7 @@ packages_conf() {
         ["nvm"]="curl:nvm"
         ["go"]="apt:golang-go curl:go"
         ["gopls"]="go:gopls"
+        ["keychain"]="pacman:keychain"
         ["unzip"]="pacman:unzip"
         ["gcc"]="pacman:gcc"
         ["openssh"]="pacman:openssh"
@@ -251,7 +252,7 @@ packages_conf() {
     }
 
     # Default packages to install
-    local packages_to_install=("node" "npm" "nvm" "go" "gopls", "unzip", "gcc", "openssh", "wget", "eza")
+    local packages_to_install=("node" "npm" "nvm" "go" "gopls" "keychain" "unzip" "gcc" "openssh" "wget" "eza")
     
     # Allow custom package list from package arguments
     if [ ${#package_args[@]} -gt 0 ]; then
