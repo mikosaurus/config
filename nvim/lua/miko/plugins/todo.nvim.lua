e74ad652db954b8ae96ab9d2ecc5cbd1b75c058b
Anders Johannessen <slatleim1991@gmail.com>
2026-02-27 09:52:01 +0100

    added new command

--- nvim/lua/miko/plugins/todo.nvim.lua
+++ nvim/lua/miko/plugins/todo.nvim.lua
@@ -1,6 +1,5 @@
 return {
 	"mikosaurus/todo.nvim",
-	-- dir = "~/github.com/mikosaurus/todo.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
 		local todofiles = require("todofiles")
@@ -20,6 +19,7 @@ return {
 				vim.keymap.set("n", "<M-n>", todofiles.set_task_open, { buffer = true })
 				vim.keymap.set("n", "<M-d>", todofiles.set_task_done, { buffer = true })
 				vim.keymap.set("n", "<M-c>", todofiles.set_task_cancelled, { buffer = true })
+				vim.keymap.set("n", "<M-s>", todofiles.set_task_started, { buffer = true })
 				vim.keymap.set("n", "<M-N>", todofiles.clear_task, { buffer = true })
 			end,
 		})
