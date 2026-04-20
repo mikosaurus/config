a37764c5b335e3bbf1b63cb848588ac5c19331ce
Anders Johannessen <slatleim1991@gmail.com>
2026-04-20 15:20:25 +0200

    improvements

--- nvim/lua/miko/plugins/telescope.lua
+++ nvim/lua/miko/plugins/telescope.lua
@@ -1,33 +1,34 @@
 return {
-	"nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope.nvim",
 
-	branch = "master",
+    branch = "master",
 
-	dependencies = { "nvim-lua/plenary.nvim", "benfowler/telescope-luasnip.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim", "benfowler/telescope-luasnip.nvim" },
 
-	config = function()
-		local telescope = require("telescope")
-		telescope.setup({})
+    config = function()
+        local telescope = require("telescope")
+        telescope.setup({})
 
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find files in git repo" })
-		vim.keymap.set("n", "<leader>fw", function()
-			local word = vim.fn.expand("<cword>")
-			builtin.grep_string({ search = word })
-		end)
-		vim.keymap.set("n", "<leader>fW", function()
-			local word = vim.fn.expand("<cWORD>")
-			builtin.grep_string({ search = word })
-		end)
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find files in git repo" })
+        vim.keymap.set("n", "<leader>fw", function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set("n", "<leader>fW", function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
 
-		vim.keymap.set("n", "<leader>fs", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		vim.keymap.set("n", "<leader>fb", builtin.current_buffer_fuzzy_find)
-		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>fs", function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set("n", "<leader>fb", builtin.current_buffer_fuzzy_find)
+        vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
 
-		telescope.load_extension("noice")
-		telescope.load_extension("luasnip")
-	end,
+        telescope.load_extension("noice")
+        telescope.load_extension("luasnip")
+    end,
 }
