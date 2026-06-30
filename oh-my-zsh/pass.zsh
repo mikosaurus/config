2371b7f82a288239f5d02370d111bdfb47d643f0
Anders Johannessen <slatleim1991@gmail.com>
2026-06-30 10:35:47 +0200

    updated pass.zsh

--- oh-my-zsh/pass.zsh
+++ oh-my-zsh/pass.zsh
@@ -1,4 +1,24 @@
 # At new zsh, always do pass git pull to have newest changes
-if command -v pass > /dev/null 2>&1; then
-    pass git pull --quiet
+update_pass() {
+  pass git pull --quiet
+}
+
+if ! command -v nslookup > /dev/null 2>&1; then
+    echo "missing nslookup, cannot check if dns server has git server entry"
+    return
 fi
+
+if [ ! -f ~/.mks/pass.cache ] && [ $(( $(date +%s) - $(date -r ~/.mks/pass.cache +%s) )) -le 86400 ]; then
+  update_pass
+  return
+fi
+
+if ! nslookup ssh.git.353.no > /dev/null 2>&1; then
+  echo "unable to talk to local git server"
+else
+  mkdir -p ~/.mks
+  date > ~/.mks/pass.cache
+  update_pass
+fi
+
+
